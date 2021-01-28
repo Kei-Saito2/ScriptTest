a16aa42248fe1a4088f67371b860a833e671918c
Kei-Saito2 <77094528+Kei-Saito2@users.noreply.github.com>
2021-01-28 22:44:47 +0900

    課題：for文の使用、発展課題：Magic関数の修正
    ○課題
    　配列の表示をfor文を使用して修正しました。
    ○発展課題
    　Magic関数内でmp残量が足りているのか判定する前にmpを減らしていたため、修正しました。
    　複数回の同じ記載をfor文を使用して修正しました。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -25,18 +25,17 @@ public class Boss
 
     public void Magic()
     {
-        this.mp = mp - magic;
-        if (this.mp >= 0)
+        if (this.mp >= magic)
         {
+            this.mp = mp - magic;
             Debug.Log("魔法攻撃をした。残りMPは" + this.mp + "。");
         }
-        else if (this.mp < 0)
+        else
         {
             Debug.Log("MPが足りないため魔法が使えない。");
         }
     }
 }
-
 
 public class Test : MonoBehaviour
 {
@@ -44,36 +43,26 @@ public class Test : MonoBehaviour
     {
         //課題
         int[] array = { 1, 2, 3, 4, 5 };
-        Debug.Log(array[0]);
-        Debug.Log(array[1]);
-        Debug.Log(array[2]);
-        Debug.Log(array[3]);
-        Debug.Log(array[4]);
+        for(int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
 
         Array.Reverse(array);
-        Debug.Log(array[0]);
-        Debug.Log(array[1]);
-        Debug.Log(array[2]);
-        Debug.Log(array[3]);
-        Debug.Log(array[4]);
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
 
         //発展課題
         Boss lastboss = new Boss();
         lastboss.Attack();
         lastboss.Defence(3);
-        lastboss.Magic();
-        lastboss.Magic();
-        lastboss.Magic();
-        lastboss.Magic();
-        lastboss.Magic();
-        lastboss.Magic();
-        lastboss.Magic();
-        lastboss.Magic();
-        lastboss.Magic();
-        lastboss.Magic();
-        lastboss.Magic();
-        lastboss.Magic();
-
+        for (int i = 0; i < 12; i++)
+        {
+            lastboss.Magic();
+        }
+   
         Boss midboss = new Boss();
         midboss.Attack();
         midboss.Defence(5);
